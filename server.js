6f1d42c96571457ddd2735d7787140788d79d092
Phil Mach <philmach2@icloud.com>
2023-11-06 12:30:46 -0500

    added comment in server.js to connect DB

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const connectDB = require('./config/database')
 require('dotenv').config({path: './config/.env'})
 
 //DB function is called to connect to DB w Mongoose
+//*Set up connection in config/database.js
 // connectDB()
 
 //using ejs view engine, can change to any front end framework
