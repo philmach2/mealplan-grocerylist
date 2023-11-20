c6fea08a04412ff7b83e0e84c29006ea75db352b
Phil Mach <110493891+philmach2@users.noreply.github.com>
2023-11-20 12:50:55 -0500

    BIG CHANGE, changed DB connection to server.js to avoid confusion, database.js is not IRRELIVANT

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express')
 const app = express()
-const connectDB = require('./config/database')
+const MongoClient = require('mongodb').MongoClient
 require('dotenv').config({path: './config/.env'})
-//DB function is called to connect to DB w Mongoose
-//*Set up connection in config/database.js
-connectDB()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+let db,
+dbConnectionStr = process.env.DB_STRING,
+dbName = 'meal-plan-grocery-list-database'
+
+MongoClient.connect(dbConnectionStr)
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+        //to access grocery-list-collection in DB
+        groceryListCollection = db.collection('entries')
+}) 
 
 
 // MEAL PLAN
