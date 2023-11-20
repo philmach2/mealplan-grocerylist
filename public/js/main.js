441f6beed0516a043b7e095702418ef72b984bcd
Phil Mach <110493891+philmach2@users.noreply.github.com>
2023-11-20 12:37:51 -0500

    deleted experimental, commented out code in main.js

--- public/js/main.js
+++ public/js/main.js
@@ -4,11 +4,3 @@ const addItem = document.querySelector('#number-item-button')
 addItem.addEventListener('click', _ => {
     //want to send post request on click
 })
-
-// const switchPage = document.querySelector('#switch-page')
-
-// switchPage.addEventListener('click', idk => {
-//     app.get('/meal-plan', (req, res) => {
-//         res.render('meal-plan.ejs')
-//     })
-// })
