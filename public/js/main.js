f4d2c81d414de894452ce8f655d2c786af0ca041
Phil Mach <110493891+philmach2@users.noreply.github.com>
2023-11-20 12:17:26 -0500

    added forms to g-list.ejs, added body-parser to .json, added post method server.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 //variable for DOM buttons (grocery-list.ejs)
-const addItem = document.querySelector('#add-item-button')
+const addItem = document.querySelector('#number-item-button')
 
 addItem.addEventListener('click', _ => {
     //want to send post request on click
