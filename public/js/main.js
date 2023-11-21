72cdc68752841bcd258f3f627484992951ed7e93
Phil Mach <110493891+philmach2@users.noreply.github.com>
2023-11-21 17:54:13 -0500

    added + button to ejs

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
-//variable for DOM buttons (grocery-list.ejs)
-const numOfItems = document.querySelector('#number-item-button')
+//Grocery List Update number of item
+const numOfItem = document.querySelector('#number-item-button')
 
-numOfItems.addEventListener('click', _ => {
-    //want to send post request on click
+numOfItem.addEventListener('click', _ => {
+    
 })
