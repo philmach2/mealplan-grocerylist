9ce612c5e0f117815cd1c4c011814171ff02ca73
Phil Mach <110493891+philmach2@users.noreply.github.com>
2023-11-27 12:29:00 -0500

    added hidden input to g-list ejs forms, playing w PUT request and fetch

--- public/js/main.js
+++ public/js/main.js
@@ -3,13 +3,15 @@
 const addNumOfItem = document.querySelector('#add-num-button')
 const subNumOfItem = document.querySelector('#sub-num-button')
 const insertNum = Number()
+// const tLikes = Number(this.parentNode.childNodes[5].innerText)
 
 addNumOfItem.addEventListener('click', _ => {
+    const itemCategory = 
     fetch('/addNum', {
         method: 'put',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
-
+            // numItem: tLikes
         })
     })
 })
