5662fc239af440f87cdad597da6546596e95a17c
Phil Mach <110493891+philmach2@users.noreply.github.com>
2023-11-28 17:54:18 -0500

    completed install of TailWind and DaisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './index.html',
+    './views/*.ejs',
+    './public/js/main.js',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
-
