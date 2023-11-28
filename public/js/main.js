0301d8d01fc7ccc61990a2e77a4aa6a5edb18f2e
Phil Mach <110493891+philmach2@users.noreply.github.com>
2023-11-28 10:54:49 -0500

    addNum PUT is working, but not printing to DOM yet, also added hidden input ejs

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,55 @@
 // need to figure out how to add num and sub num of items, both for data in db and text on page
 
-const addNumOfItem = document.querySelector('#add-num-button')
-const subNumOfItem = document.querySelector('#sub-num-button')
-const insertNum = Number()
-// const tLikes = Number(this.parentNode.childNodes[5].innerText)
+const increase = document.querySelectorAll('#add-num-button')
+const decrease = document.querySelectorAll('#sub-num-button')
 
-addNumOfItem.addEventListener('click', _ => {
-    const itemCategory = 
-    fetch('/addNum', {
-        method: 'put',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            // numItem: tLikes
-        })
-    })
+Array.from(increase).forEach((element)=>{
+    element.addEventListener('click', addNumOfItem)
 })
 
-subNumOfItem.addEventListener('click', _ => {
-    fetch('/subNum', {
-        method: 'put',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
+// Array.from(decrease).forEach((element)=>{
+//     element.addEventListener('click', subNumOfItem)
+// })
+
+// addNumOfItem.addEventListener('click', _ => {
+//     const itemCategory = 
+//     fetch('/addNum', {
+//         method: 'put',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify({
+//             // numItem: tLikes
+//         })
+//     })
+// })
+
+
+async function addNumOfItem(){
+    const itemName = this.parentNode.childNodes[1].innerText
+    const numItem = Number(this.parentNode.childNodes[5].innerText)
+    try{
+        const response = await fetch('addNum', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'itemName': itemName,
+              'numItem': numItem
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+// subNumOfItem.addEventListener('click', _ => {
+//     fetch('/subNum', {
+//         method: 'put',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify({
             
-        })
-    })
-})
+//         })
+//     })
+// })
