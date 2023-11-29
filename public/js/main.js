d737e93201c5a52549367c5400dda83490fbffda
Chris Nelson <chrisnelson7791@gmail.com>
2023-11-28 22:20:23 -0800

    Trying to add delete functionality. I messed with a lot of things and commented out a lot of stuff in files including meal-plan, main. grocery-list, and server. Delete functionality does not work yet

--- public/js/main.js
+++ public/js/main.js
@@ -5,23 +5,59 @@ const subNumOfItem = document.querySelector('#sub-num-button')
 const insertNum = Number()
 // const tLikes = Number(this.parentNode.childNodes[5].innerText)
 
-addNumOfItem.addEventListener('click', _ => {
-    const itemCategory = 
-    fetch('/addNum', {
-        method: 'put',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            // numItem: tLikes
-        })
-    })
+// addNumOfItem.addEventListener('click', _ => {
+//     const itemCategory = 
+//     fetch('/addNum', {
+//         method: 'put',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify({
+//             // numItem: tLikes
+//         })
+//     })
+// })
+
+// subNumOfItem.addEventListener('click', _ => {
+//     fetch('/subNum', {
+//         method: 'put',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify({
+            
+//         })
+//     })
+// })
+
+
+// ~~~~~~~~~
+// Meal Plan Delete Functionality
+// ~~~~~~~~~
+const deleteBtn = document.querySelectorAll('.fa-trash')
+
+Array.from(deleteBtn).forEach((element)=>{
+    element.addEventListener('click', deleteItem)
 })
 
-subNumOfItem.addEventListener('click', _ => {
-    fetch('/subNum', {
-        method: 'put',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            
-        })
-    })
-})
+// I was able to get this to work when i comment out the async function deleteItem. So i know the event listeners are being added
+// function deleteItem() {
+//     console.log('hello')
+// }
+
+
+async function deleteItem(){
+    const itemText = this.parentNode.childNodes[1].innerText
+    try{
+        const response = await fetch('/deleteItem', {
+            method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'itemFromJS': itemText
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
