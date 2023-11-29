b46a1dce6d757022799ca0a6eb8b17d1da3465e6
Phil Mach <110493891+philmach2@users.noreply.github.com>
2023-11-29 18:17:55 -0500

    successfully connected CSS by omitting PUBLIC, successfully implemented PUT markComplete to PRODUCE section

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,21 @@
 const increase = document.querySelectorAll('#add-num-button')
 const decrease = document.querySelectorAll('#sub-num-button')
+const item = document.querySelectorAll('.item span')
+
+// const itemCompleted = document.querySelectorAll('.item span.completed')
+// const deleteBtn = document.querySelectorAll('.fa-trash')
+
+// Array.from(deleteBtn).forEach((element)=>{
+//     element.addEventListener('click', deleteItem)
+// })
+
+Array.from(item).forEach((element)=>{
+    element.addEventListener('click', markComplete)
+})
+
+// Array.from(itemCompleted).forEach((element)=>{
+//     element.addEventListener('click', markUnComplete)
+// })
 
 Array.from(increase).forEach(element => {
     element.addEventListener('click', addNumOfItem)
@@ -42,6 +58,25 @@ async function subNumOfItem(){
             body: JSON.stringify({
               'itemName': itemNameS,
               'numItem': numItemS
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function markComplete(){
+    const itemText = this.parentNode.childNodes[1].innerText
+    try{
+        const response = await fetch('markComplete', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'itemFromJS': itemText
             })
           })
         const data = await response.json()
